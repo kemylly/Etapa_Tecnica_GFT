285c35246e207f73fbcae3af69f23a237b053d72
kemylly <kemylly@users.noreply.github.com>
2020-09-15 10:44:15 -0300

    Realizaei exercicio 3

---
+++ Exercicio3/Exercicio3/Pessoa.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class Pessoa
+    {
+		//construtores
+		public Pessoa ()
+		{
+			this.Dia = 0;
+			this.Mes = 0;
+			this.Ano = 0;
+		}
+
+		public Pessoa (int dia, int mes, int ano) //sobrecarga
+		{
+			this.Dia = dia;
+			this.Mes = mes;
+			this.Ano = ano;
+		}
+
+		//propriedades
+		private int dia;
+
+		public int Dia
+		{
+			get { return dia; }
+			set { dia = value; }
+		}
+
+		private int mes;
+
+		public int Mes
+		{
+			get { return mes; }
+			set { mes = value; }
+		}
+
+		private int ano;
+
+		public int Ano
+		{
+			get { return ano; }
+			set { ano = value; }
+		}
+
+		//metodo
+		public void ExibirValidade()
+		{
+			if ((dia > 31) || (mes > 12) || (ano > 2018))
+			{
+				Console.WriteLine("Data invalida");
+			}
+			else
+			{
+				Console.WriteLine("Data Valida"); 
+			}
+		}
+	}
+}
---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            Console.WriteLine("Data do seu Aniversario");
+            Console.WriteLine("*Digite apensas numeros*\n");
+
+            Console.Write("Digite o dia: ");
+            p.Dia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o mes: ");
+            p.Mes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o ano: ");
+            p.Ano = Convert.ToInt32(Console.ReadLine());
+
+            p.ExibirValidade();  //exibir metodo
+
+            Console.ReadKey();
+        }
+    }
+}
