df507bc6007d0ba94814545b56df61f6adf639f9
kemylly <kemylly@users.noreply.github.com>
2020-09-15 11:35:18 -0300

    Adicionei o exercicio 4

---
+++ Exercicio4/Exercicio4/Elefante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    public class Elefante
+    {
+		private int num;
+
+		public int Num
+		{
+			get { return num; }
+			set { num = value; }
+		}
+
+		//metodo
+		public void ExibirElefante()
+		{
+			Console.WriteLine("Um elefente... ");
+			int i = 0;
+			while(i <= this.num)
+			{
+				Console.Write(" Incomoda ");
+				i++;
+			}
+		}
+	}
+}
---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elefante f = new Elefante();
+
+            Console.WriteLine("Digite um numero para o elefnate");
+            f.Num = Convert.ToInt32(Console.ReadLine());
+
+            f.ExibirElefante();
+        }
+    }
+}
